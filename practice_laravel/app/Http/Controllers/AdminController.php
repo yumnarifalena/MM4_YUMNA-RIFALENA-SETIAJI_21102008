1d398c8e45c940a419e0d3fd09d41d33d5165505
yumnarifalena <rifalenasetiaji@gmail.com>
2024-02-02 22:32:42 +0700

    commit

---
+++ practice_laravel/app/Http/Controllers/AdminController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Admin;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('login.login');
+    }
+    public function process(Request $request)
+    {
+        $validateData = $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+        $result = Admin::where('username', '=', $validateData['username'])->first();
+        if ($result) {
+            if (($request->password == $result->password)) {
+                session(['username' => $request->username]);
+                return redirect('/student');
+            } else {
+                return back()->withInput()->with('pesan', "Login Gagal");
+            }
+        } else {
+            return back()->withInput()->with('pesan', "Login Gagal");
+        }
+    }
+    public function logout()
+    {
+        session()->forget('username');
+        return redirect('login')->with('pesan', 'Logout berhasil');
+    }
+}
